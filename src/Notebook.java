c2be26b1618ca7d51338f93df79ec869f670e13c
Ablingis Dominykas <dominykas.ablingis@mif.stud.vu.lt>
2015-03-18 18:51:28 +0200

    removed old todo list

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,6 +1,8 @@
 /**
  * Created by Dominykas Ablingis
  */
+
+//test
 public class Notebook {
     public static void main(String[] args){
         if ((args.length == 1) && args[0].equals("test")) {
